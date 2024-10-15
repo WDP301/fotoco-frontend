cd655b537979d2cbc8005f737ae4aa967b507d7d
Minh <minhht.1.2.vn@gmail.com>
2024-10-15 23:13:29 +0700

    fix: create album not close dialog

--- src/components/overview/group/album-section/create-album-dialog.tsx
+++ src/components/overview/group/album-section/create-album-dialog.tsx
@@ -1,4 +1,5 @@
-'use client'
+'use client';
+
 import {
   Dialog,
   DialogContent,
@@ -6,34 +7,34 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import CreateAlbumForm from "./create-album-form"
-import { useLanguage } from "@/components/provider/language-provider"
-import { Button } from "@/components/ui/button";
-import { Images } from "lucide-react";
+} from '@/components/ui/dialog';
+import CreateAlbumForm from './create-album-form';
+import { useLanguage } from '@/components/provider/language-provider';
+import { Button } from '@/components/ui/button';
+import { Images } from 'lucide-react';
+import { useState } from 'react';
 
 export default function CreateAlbumDiaLog({ groupId }: { groupId: string }) {
+  const [open, setOpen] = useState(false);
+  const { dict } = useLanguage();
 
-    const { dict } = useLanguage();
-    
-
-    return (
-        <Dialog>
-        <DialogTrigger asChild>
-            <Button variant="ghost">
-                <Images className="mr-2 h-4 w-4" />
-                {dict.button.createAlbum}
-            </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-            <DialogTitle>{dict.createAlbum.dialogTitle}</DialogTitle>
-            <DialogDescription>
-                {dict.createAlbum.dialogDescription}
-            </DialogDescription>
-            </DialogHeader>
-            <CreateAlbumForm setOpen={() => {}} groupId={groupId} />
-        </DialogContent>
-        </Dialog>
-  )
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant="ghost">
+          <Images className="mr-2 h-4 w-4" />
+          {dict.button.createAlbum}
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>{dict.createAlbum.dialogTitle}</DialogTitle>
+          <DialogDescription>
+            {dict.createAlbum.dialogDescription}
+          </DialogDescription>
+        </DialogHeader>
+        <CreateAlbumForm setOpen={setOpen} groupId={groupId} />
+      </DialogContent>
+    </Dialog>
+  );
 }

7e3bfe230e8a2cec978dbe470789abc2315c3c60
Minh <minhht.1.2.vn@gmail.com>
2024-10-18 17:20:44 +0700

    style: update how photo show in photo list

--- src/components/overview/album/photo-list.tsx
+++ src/components/overview/album/photo-list.tsx
@@ -1,35 +1,20 @@
-import PhotoItem from './photo-item';
-import { Photo, SearchPhotoParams } from '@/lib/define';
-import ListPagination from '@/components/shared/list-pagination';
+// PhotoList.tsx (Server Component)
 import { getPhotosByAlbumId } from '@/lib/data';
 import { getDictionary } from '@/lib/dictionaries';
+import { SearchPhotoParams } from '@/lib/define';
+import ListPagination from '@/components/shared/list-pagination';
+import PhotoListView from './photo-list-view';
 
-const buildPhotoItems = (photos: Photo[]) => {
-  const items = [];
-  for (let index = 1; index < 5; index++) {
-    items.push(
-      <div key={index} className="flex flex-col justify-start gap-4">
-        {photos
-          .filter((photo, photoIndex) => {
-            return photoIndex % 4 === index - 1;
-          })
-          .map((filteredPhoto) => (
-            <PhotoItem key={filteredPhoto._id} photo={filteredPhoto} />
-          ))}
-      </div>
-    );
-  }
-  return items;
-};
-const PhotoList = async ({
+export default async function PhotoList({
   albumId,
   searchParams,
 }: {
   albumId: string;
   searchParams: SearchPhotoParams;
-}) => {
+}) {
   const dict = await getDictionary();
   const { photos, pageMeta } = await getPhotosByAlbumId(albumId, searchParams);
+
   if (photos.length === 0) {
     return (
       <div className="flex justify-center items-center h-96">
@@ -37,16 +22,13 @@ const PhotoList = async ({
       </div>
     );
   }
+
   return (
     <>
-      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4 mt-5">
-        {buildPhotoItems(photos)}
-      </div>
+      <PhotoListView photos={photos} />
       <div className="my-3">
         <ListPagination meta={pageMeta} bookmark="album-name" />
       </div>
     </>
   );
-};
-
-export default PhotoList;
+}

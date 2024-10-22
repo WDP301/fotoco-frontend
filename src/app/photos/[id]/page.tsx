b4222246bc5d751cb35e50b9a2c88272ce749dde
Minh <minhht.1.2.vn@gmail.com>
2024-10-22 12:00:20 +0700

    feat: add gallery view to photo detail

--- src/app/photos/[id]/page.tsx
+++ src/app/photos/[id]/page.tsx
@@ -1,14 +1,15 @@
-import PhotoNotFound from "@/components/overview/photo/photo-not-found";
-import PhotoCarousel from "@/components/overview/photo/photo-section/photo-carousel"
-import PostSection from "@/components/overview/photo/post-section/post-section"
-import { getPhotoDetails } from "@/lib/data";
-import { SearchPhotoParams } from "@/lib/define";
-import { Metadata } from "next";
+import PhotoNotFound from '@/components/overview/photo/photo-not-found';
+import PhotoCarousel from '@/components/overview/photo/photo-section/photo-carousel';
+import PostSection from '@/components/overview/photo/post-section/post-section';
+import { siteConfig } from '@/config/site';
+import { getPhotoDetails } from '@/lib/data';
+import { SearchPhotoParams } from '@/lib/define';
+import { Metadata } from 'next';
 
 type Pros = {
-  params: {id: string};
-  searchParams: SearchPhotoParams
-}
+  params: { id: string };
+  searchParams: SearchPhotoParams;
+};
 
 export async function generateMetadata({
   params,
@@ -17,41 +18,43 @@ export async function generateMetadata({
   const photo = await getPhotoDetails(params.id, searchParams);
   if (!photo || !photo.photo._id) {
     return {
-      title: "Photo Not Found",
-      description: "Photo Not Found",
+      title: 'Photo Not Found',
+      description: 'Photo Not Found',
     };
   }
 
   return {
-    title: `${photo.photo.title} - ${photo.photo.owner.fullName}`,
+    title: {
+      default: `${photo.photo.title ? photo.photo.title : 'Photo'} - ${photo.photo.owner.fullName} | ${siteConfig.name}`,
+      template: `%s | ${siteConfig.name}`,
+    },
     description: `${photo.photo.title}`,
-  }
+  };
 }
 export default async function PhotoDetailsPage({
   params,
   searchParams,
 }: {
   params: { id: string };
-  searchParams: SearchPhotoParams
+  searchParams: SearchPhotoParams;
 }) {
-
   const photo = await getPhotoDetails(params.id, searchParams);
   if (!photo || !photo.photo._id) {
-    return <PhotoNotFound />
-  }  
+    return <PhotoNotFound />;
+  }
 
   return (
-      <div className="max-w-screen h-screen overflow-hidden flex">
-        <div className="flex flex-col justify-between w-3/4">
-          <div className="w-full grid gap-1.5">
-            <PhotoCarousel photo={photo} searchParams={searchParams}/>
-          </div>
-        </div>
-        <div className="flex flex-col justify-between w-1/4 p-4">
-          <div className="w-full grid gap-1.5">
-            <PostSection photo = {photo} />
-          </div>
+    <div className="max-w-screen h-screen overflow-hidden flex">
+      <div className="flex flex-col justify-between w-3/4">
+        <div className="w-full grid gap-1.5">
+          <PhotoCarousel photo={photo} searchParams={searchParams} />
         </div>
       </div>
-  )
+      <div className="flex flex-col justify-between w-1/4 p-4">
+        <div className="w-full grid gap-1.5">
+          <PostSection photo={photo} />
+        </div>
+      </div>
+    </div>
+  );
 }

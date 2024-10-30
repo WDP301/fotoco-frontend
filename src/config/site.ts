f787950fb2558bc48db7508674bf493f13d13377
Minh <minhht.1.2.vn@gmail.com>
2024-10-30 17:20:19 +0700

    chore: update site config

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: process.env.FRONTEND_SITE_NAME || 'Fotoco',
   url: process.env.FRONTEND_URL || 'http://localhost:3000',
-  baseApiURL: process.env.API_URL || 'http://localhost:9999',
+  baseApiURL: process.env.API_URL || 'http://localhost:4000/api/v1',
 };
 
 export type SiteConfig = typeof siteConfig;
